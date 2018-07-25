51a1d1b05a8ead39d38d5768b12559321fcf10fd
matanui159 <josh@minteronline.com>
2018-07-25 10:08:47 +1000

    Changed title

--- conf.lua
+++ conf.lua
@@ -3,7 +3,7 @@ HEIGHT = 48
 SCALE  = 12
 
 function love.conf(config)
-	config.window.title = "COFFEE RUN"
+	config.window.title = "Coffee Run"
 	config.window.width = WIDTH * SCALE
 	config.window.height = HEIGHT * SCALE
 end
